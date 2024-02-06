570ff36a274b0a1a6dd308f9d88cb69cc318fcbb
DaniAm94 <dani.amico94@gmail.com>
2024-02-06 15:45:34 +0100

    created app header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'App Header'
+}
+</script>
+
+<template>
+    <h1>Pokedex</h1>
+    <select name="type" id="type">
+        <option selected value="All">All</option>
+        <option value="saab">Saab</option>
+
+    </select>
+</template>
